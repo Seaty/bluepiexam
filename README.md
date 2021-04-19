5fb278c980eeb0e53872203bcffb8de6d8a4cf8e
seatysw <seaty.sw@gmail.com>
2021-04-19 17:31:37 +0700

    update readme

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ openssl rand -hex 32
 ```
  จากนั้นในไปใส่ใน File .env
 ```bash
-SECRET_KEY=<Hex String ที่สร้างมา>
+SECRET_KEY=Hex String ที่สร้างมา
 ```
 
 ## ขั้นตอนการติดตั้ง
@@ -21,14 +21,19 @@ SECRET_KEY=<Hex String ที่สร้างมา>
 4. ใช้คำสั่ง docker-compose up
 5. สามารถใช้ API ได้จากพอร์ท 8000 ได้เลย
 
-## ข้อมูล API
+## ข้อมูลการใช้งาน API
 
-| API Name | Method | คำอธิบาย | ค่าที่ส่งใน body/request |
-| ------ | ------ | ------ | ------ |
-| /get_token | POST | เป็น API ที่ส่งค่า username มาเพื่อที่รับ Token ใช้ในการยืนยันตัวตนในระบบ | {"username":<username>} |
-| /start_new_game | POST | เป็น API สำหรับใช้กับปุ่มเริ่มเล่นเกมโดยการส่ง Token ที่ได้จาก /get_token ใน Authentication Header |  |
+| API Name | Method | คำอธิบาย | ค่าที่ส่งใน body/request | จำเป้นต้องใส่ Token ใน Header |
+| ------ | ------ | ------ | ------ | ------ |
+| /get_token | POST | เป็น API ที่ส่งค่า username มาเพื่อที่รับ Token ใช้ในการยืนยันตัวตนในระบบ | {"username":ชื่อusername} | ไม่ |
+| /start_new_game | POST | เป็น API สำหรับใช้กับปุ่มเริ่มเล่นเกมโดยการส่ง | - | จำเป็น |
 
-## ข้อมูล Web Socket
+## ข้อมูลการใช้งาน Web Socket
 
-| API Name | Method | คำอธิบาย | ค่าที่ส่งใน body/request |
-| ------ | ------ | ------ | ------ |
+| Socket Name | คำอธิบาย |
+| ------ | ------ |
+| /playgame | เป็น WebSocket ที่ติดต่อส่งข้อมูลในระหว่างเกม |
+
+โดยการใช้งานของ Websocket นี้จะเป้นขั้นตอนดังนี้
+
+1. ให้ส่งค่า Token ไปเพื่อทำการเริ่มการก่อนระบบจะเช็ค Username จาก Token แล้วส่งค่า Global Score และ Best score ของผู้เล่นนั้นมา (ซึ่งในจุดนี้แปลว่า คุณได้ทำการเชื่อมต่อ Websocket สำเร็จแล้ว)
--- service/main.py
+++ service/main.py
@@ -1,9 +1,9 @@
-from typing import List,Optional
+from typing import List, Optional
 from fastapi import Depends, FastAPI,  WebSocket, WebSocketDisconnect
 from sqlalchemy.orm import Session
 from . import crud, models, schemas, auth
 from .database import SessionLocal, engine, get_db
-from datetime import timedelta,datetime
+from datetime import timedelta, datetime
 from .settings import ACCESS_TOKEN_EXPIRE_MINUTES
 
 models.Base.metadata.create_all(bind=engine)
@@ -12,6 +12,7 @@ app = FastAPI()
 
 
 """================== Api Zone ================== """
+
 
 @app.post("/get_token", response_model=schemas.Token)
 def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
@@ -24,6 +25,7 @@ def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+
 @app.get("/start_new_game")
 def new_game(user=Depends(auth.get_current_user), db: Session = Depends(get_db)):
     crud.start_game(db, user)
@@ -31,6 +33,7 @@ def new_game(user=Depends(auth.get_current_user), db: Session = Depends(get_db))
 
 
 """ ================== Web socket Zone ================== """
+
 
 class ConnectionManager:
     def __init__(self):
@@ -50,13 +53,15 @@ class ConnectionManager:
         for connection in self.active_connections:
             await connection.send_text(message)
 
+
 manager = ConnectionManager()
+
 
 async def update_best_score(websocket, user, db):
     try:
-        player_best_score = crud.get_player_best_score(db,user)
-        global_best_score =  crud.get_global_best_score(db)
-        await manager.broadcast(f"Global:{best_score}")
+        player_best_score = crud.get_player_best_score(db, user)
+        global_best_score = crud.get_global_best_score(db)
+        await manager.broadcast(f"Global:{global_best_score}")
         await manager.send_personal_message(f"Score:{player_best_score}", websocket)
     except:
         pass
@@ -70,31 +75,32 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)
         token = await websocket.receive_text()
         user = await auth.get_current_user(token, db)
         # อัพเดทคะแนนสูงสุดของผู้เล่นและ Global
-        await update_best_score(websocket,user,db)
+        await update_best_score(websocket, user, db)
         # เริ่มเกม
         while True:
-            #รับข้อมูลจาก client
+            # รับข้อมูลจาก client
             data = await websocket.receive_text()
-            #แยกส่วนข้อมูลเพื่อทำการเช็ค
+            # แยกส่วนข้อมูลเพื่อทำการเช็ค
             split_data = data.split(":")
             if split_data[0] == "click":
-                #ข้อมุลตำแหน่งที่คลิก
+                # ข้อมุลตำแหน่งที่คลิก
                 pos = int(split_data[1])
                 value, clicks, solved = crud.card_click(db, user, pos)
-                gameResponse = schemas.GameReponse(pos=pos,value=value,clicks=clicks)
-                await manager.send_personal_message(gameResponse.dict(),websocket)
+                gameResponse = schemas.GameReponse(
+                    pos=pos, value=value, clicks=clicks)
+                await manager.send_personal_message(gameResponse.dict(), websocket)
                 # เช็คว่าเปิดครบทุกใบแล้วถูกต้อง
                 if solved:
                     # อัพเดทคะแนนสูงสุดของผู้เล่นและ Global
-                    await update_best_score(websocket,user,db)
+                    await update_best_score(websocket, user, db)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
     except Exception as err:
         print(err)
-        #ดัก Error ที่ตั้งขึ้นด้วยตัวเอง
+        # ดัก Error ที่ตั้งขึ้นด้วยตัวเอง
         if hasattr(err, 'detail'):
             await manager.send_personal_message(f"Error: {err.detail}", websocket)
-        #ดัก Error ที่เกิดขึ้นเองในโปรแกรม
+        # ดัก Error ที่เกิดขึ้นเองในโปรแกรม
         else:
             await manager.send_personal_message(f"Error: {err.args[0]}", websocket)
         manager.disconnect(websocket)
